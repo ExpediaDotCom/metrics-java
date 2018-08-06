fd96efe3eaffbf5e697624e1f3eac4449c675a36
Peter Hall <phall@expedia.com>
2018-08-07 09:48:40 +1000

    Remove www from group and package name

--- src/main/java/com/expedia/metrics/MessagePackSerializer.java
+++ src/main/java/com/expedia/metrics/MessagePackSerializer.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.expedia.www.metrics;
+package com.expedia.metrics;
 
 import org.msgpack.core.MessageBufferPacker;
 import org.msgpack.core.MessagePack;
--- src/test/scala/com/expedia/metrics/MetricDataTest.scala
+++ src/test/scala/com/expedia/metrics/MetricDataTest.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.expedia.www.metrics
+package com.expedia.metrics
 
 import org.scalatest.{FunSpec, GivenWhenThen, Matchers}
 import scala.collection.JavaConverters._
